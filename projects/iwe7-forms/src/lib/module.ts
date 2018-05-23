b5e7a0e2af4e45573939edb814168cc913bcc7bf
iwe7 <admin@meepo.com.cn>
2018-05-23 22:23:16 +0800

    save

---
+++ projects/iwe7-forms/src/lib/module.ts
@@ -0,0 +1,54 @@
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { DelonUtilModule } from "@delon/util";
+
+import { DelonFormConfig } from "./config";
+import {
+  SchemaValidatorFactory,
+  AjvSchemaValidatorFactory
+} from "./validator.factory";
+import { SFComponent } from "./sf.component";
+import { SFItemComponent } from "./sf-item.component";
+import { SFItemWrapComponent } from "./sf-item-wrap.component";
+import { SFTemplateDirective } from "./widgets/custom/sf-template.directive";
+import { SFFixedDirective } from "./sf-fixed.directive";
+
+const COMPONENTS = [
+  SFComponent,
+  SFItemComponent,
+  SFItemWrapComponent,
+  SFTemplateDirective,
+  SFFixedDirective
+];
+
+// region: widgets
+
+import { WidgetRegistry } from "./widget.factory";
+import { NzWidgetRegistry } from "./widgets/nz-widget.registry";
+
+const WIDGETS = [];
+
+// endregion
+
+@NgModule({
+  imports: [CommonModule, FormsModule, DelonUtilModule],
+  declarations: [...COMPONENTS, ...WIDGETS],
+  entryComponents: [...WIDGETS],
+  exports: [...COMPONENTS]
+})
+export class DelonFormModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: DelonFormModule,
+      providers: [
+        DelonFormConfig,
+        {
+          provide: SchemaValidatorFactory,
+          useClass: AjvSchemaValidatorFactory
+        },
+        { provide: WidgetRegistry, useClass: NzWidgetRegistry }
+      ]
+    };
+  }
+}
---
+++ projects/iwe7-forms/src/lib/widgets/nz-widget.registry.ts
@@ -0,0 +1,6 @@
+import { WidgetRegistry } from '../widget.factory';
+export class NzWidgetRegistry extends WidgetRegistry {
+  constructor() {
+    super();
+  }
+}
