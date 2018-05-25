89380b0a552f835a21b177168aad51bd0106adc8
iwe7 <admin@meepo.com.cn>
2018-05-25 14:31:09 +0800

    slider

--- projects/iwe7-antd-mobile/src/lib/slider/slider-handle.ts
+++ projects/iwe7-antd-mobile/src/lib/slider/slider-handle.ts
@@ -1,41 +1,103 @@
-import { Directive, Injector, Input, HostBinding, SimpleChanges, OnChanges, Output, EventEmitter, OnInit } from "@angular/core";
+import {
+  Directive,
+  Injector,
+  Input,
+  HostBinding,
+  SimpleChanges,
+  OnChanges,
+  Output,
+  EventEmitter,
+  OnInit
+} from "@angular/core";
 import { Iwe7BaseDirective } from "iwe7-base";
-import { onTouchStart, onTouchMove, onTouchEnd, onTouchCancel } from "iwe7-util";
+import {
+  onTouchStart,
+  onTouchMove,
+  onTouchEnd,
+  onTouchCancel
+} from "iwe7-util";
+import { fromEvent } from "rxjs";
+import { switchMap, map, tap, takeUntil } from "rxjs/operators";
 
 @Directive({ selector: "[am-slider-handle]" })
-export class AmSliderHandleDirective extends Iwe7BaseDirective implements OnChanges, OnInit {
+export class AmSliderHandleDirective extends Iwe7BaseDirective
+  implements OnChanges, OnInit {
   @Input() valuemin: number;
   @Input() valuemax: number;
   @Input() valuenow: number;
 
   @HostBinding("attr.role") role: string = "slider";
-  @HostBinding('style.border-color')
-  @HostBinding('style.background-color') bgColor: string = 'blue';
-  @HostBinding('style.left.%') left: number;
-  @HostBinding('style.width.px') width: number = 14;
-  @HostBinding('style.height.px') height: number = 14;
-  @HostBinding('style.margin-left.px')
-  get marginLeft(){
-    return -this.width/2;
+  @HostBinding("style.border-color")
+  @HostBinding("style.background-color")
+  bgColor: string = "blue";
+  @HostBinding("style.left.%") left: number;
+
+  @HostBinding("style.width.px") width: number = 14;
+  @HostBinding("style.height.px") height: number = 14;
+  @HostBinding("style.margin-left.px")
+  get marginLeft() {
+    return -this.width / 2;
   }
 
-  @HostBinding('style.margin-top.px')
-  get margintTop(){
+  @HostBinding("style.margin-top.px")
+  get margintTop() {
     return -4.5;
   }
 
   @Output() valuenowChange: EventEmitter<number> = new EventEmitter();
 
+  totalWidth: number;
+
   constructor(injector: Injector) {
     super(injector, "am-slider-handle");
   }
 
-  ngOnInit(){
-    
+  ngOnInit() {
+    const ele: HTMLElement = this.ele.nativeElement;
+    this.totalWidth = (<any>ele.parentNode).clientWidth;
+    let now = this.valuenow;
+    this.updateStyle();
+    onTouchStart(ele)
+      .pipe(
+        takeUntil(this.destroyed$),
+        map((res: TouchEvent) => {
+          now = this.valuenow;
+          return {
+            x: res.touches[0].pageX,
+            y: res.touches[0].pageY
+          };
+        }),
+        switchMap(start => {
+          return onTouchMove(document).pipe(
+            map(res => {
+              return {
+                x: res.touches[0].pageX,
+                y: res.touches[0].pageY
+              };
+            }),
+            map(move => {
+              return {
+                dx: move.x - start.x,
+                dy: move.y - start.y
+              };
+            }),
+            map(res => {
+              return res.dx / this.totalWidth * (this.valuemax - this.valuemin);
+            }),
+            tap(res => {
+              this.valuenow = now + res;
+              this.updateStyle();
+            })
+          );
+        })
+      )
+      .subscribe();
   }
 
-  ngOnChanges(changes: SimpleChanges){
-    this.left = (this.valuenow / (this.valuemax-this.valuemin)) * this.valuemax;
+  ngOnChanges(changes: SimpleChanges) {}
+
+  updateStyle() {
+    this.left = this.valuenow / (this.valuemax - this.valuemin) * this.valuemax;
     this.valuenowChange.emit(this.left);
   }
 }
--- projects/iwe7-antd-mobile/src/lib/slider/slider.ts
+++ projects/iwe7-antd-mobile/src/lib/slider/slider.ts
@@ -1,41 +1,56 @@
-import { Component, OnInit, Injector, Input, OnChanges, SimpleChanges } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Injector,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  Output,
+  EventEmitter
+} from "@angular/core";
 import { Iwe7BaseComponent } from "iwe7-base";
 @Component({
   selector: "am-slider",
   templateUrl: "slider.html",
   styleUrls: ["./slider.scss"]
 })
-export class AmSLiderComponent extends Iwe7BaseComponent implements OnInit, OnChanges {
+export class AmSLiderComponent extends Iwe7BaseComponent
+  implements OnInit, OnChanges {
   @Input() min: number = 0;
   @Input() max: number = 100;
   @Input() value: number = 50;
 
   @Input() height: number = 5;
 
-  @Input() activeColor: string = 'red';
-  @Input() unActiveColor: string = 'blue';
+  @Input() activeColor: string = "red";
+  @Input() unActiveColor: string = "blue";
 
   _width: number = 0;
+
+  @Output() valueChange: EventEmitter<number> = new EventEmitter();
 
   constructor(injector: Injector) {
     super(injector, "am-slider");
   }
   ngOnInit() {}
 
-  ngOnChanges(changes: SimpleChanges){
+  ngOnChanges(changes: SimpleChanges) {
     this.updateStyle();
   }
 
-  updateStyle(){
+  updateStyle() {
     this.styleObj = {
       [`active-color`]: this.activeColor,
       [`un-active-color`]: this.unActiveColor,
       width: this._width
-    }
+    };
   }
 
-  _valueChange(e: number){
+  _valueChange(e: number) {
     this._width = e;
+    const value = this._width / 100 * (this.max - this.min);
+    this.value = value;
+    this.valueChange.emit(value);
     this.updateStyle();
   }
 }
