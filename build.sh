bda1722fbc0717ddd12d0a9cbfa3359bc7f39b49
iwe7 <admin@meepo.com.cn>
2018-05-28 14:09:43 +0800

    save

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-ng build --project iwe7-dialog
-cd dist/iwe7-dialog
+ng build --project iwe7-base
+cd dist/iwe7-base
 npm publish
 cd ../
-rm -rf dist/iwe7-dialog
-yarn add iwe7-dialog
+rm -rf dist/iwe7-base
+yarn add iwe7-base
